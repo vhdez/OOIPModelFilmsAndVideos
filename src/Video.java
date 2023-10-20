b78b4fb5bec4dc8d766c48cbf759fd12279e0b9d
vhdez <vhernandez@scienceleadership.org>
2023-10-20 09:43:28 -0400

    Process all of my data strings so that they can be turned into Longs and Dates.

--- src/Video.java
+++ src/Video.java
@@ -66,22 +66,57 @@ public class Video extends Movie {
         while (dataScanner.hasNext()) {
             // construct  1 video object using 7 chunks of data
             String title = dataScanner.next();
+
             String releaseDate = dataScanner.next();
             DateTimeFormatter formatter = new DateTimeFormatterBuilder()
                     .parseCaseInsensitive()
                     .append(DateTimeFormatter.ofPattern("MMMM d, uuuu"))
                     .toFormatter(Locale.ENGLISH);            //convert String to LocalDate
             LocalDate releaseAsLocalDate = LocalDate.parse(releaseDate, formatter);
+            //Finding Nemo
+            //November 4, 2003	40,600,000	520,000[5]	38,800,000[2]	1,320,000[6]	$801,400,000[5][2][6]
 
             Long totalReleases = dataScanner.nextLong();
+
             String vhsReleases = dataScanner.next();
+            int locationOfSquareBracket = vhsReleases.indexOf("[");
+            if (locationOfSquareBracket > 0) {
+                vhsReleases = vhsReleases.substring(0, locationOfSquareBracket);
+            }
+            vhsReleases = vhsReleases.replace(",", "");
+            if (vhsReleases.equalsIgnoreCase("—")) {
+                vhsReleases = "0";
+            }
             Long vhsReleasesAsLong = Long.parseLong(vhsReleases);
+
             String dvdReleases = dataScanner.next();
+            locationOfSquareBracket = dvdReleases.indexOf("[");
+            if (locationOfSquareBracket > 0) {
+                dvdReleases = dvdReleases.substring(0, locationOfSquareBracket);
+            }
+            dvdReleases = dvdReleases.replace(",", "");
             Long dvdReleasesAsLong = Long.parseLong(dvdReleases);
+
             String blurayReleases = dataScanner.next();
+            locationOfSquareBracket = blurayReleases.indexOf("[");
+            if (locationOfSquareBracket > 0) {
+                blurayReleases = blurayReleases.substring(0, locationOfSquareBracket);
+            }
+            blurayReleases = blurayReleases.replace(",", "");
+            if (blurayReleases.equalsIgnoreCase("—")) {
+                blurayReleases = "0";
+            }
             Long blurayReleasesAsLong = Long.parseLong(blurayReleases);
+
             String revenue = dataScanner.next();
+            locationOfSquareBracket = revenue.indexOf("[");
+            if (locationOfSquareBracket > 0) {
+                revenue = revenue.substring(0, locationOfSquareBracket);
+            }
+            revenue = revenue.replace(",", "");
+            revenue = revenue.replace("$", "");
             Long revenueAsLong = Long.parseLong(revenue);
+
             // public Video(String title, long moneyEarned, LocalDate release, long allCount, long vhsCount, long dvdCount, long blurayCount) {
 
             Video newVideo = new Video(title, revenueAsLong, releaseAsLocalDate, totalReleases, vhsReleasesAsLong, dvdReleasesAsLong, blurayReleasesAsLong);
